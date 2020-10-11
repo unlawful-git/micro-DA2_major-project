e09a2f98dbee1caadf3d262816f8e28821f704df
unlawful-git <53834039+unlawful-git@users.noreply.github.com>
2020-10-11 22:36:17 +0530

    Create lcd.h

---
+++ lcd.h
@@ -0,0 +1,6 @@
+extern void lcd_init(void);
+extern void lcd_cmd(unsigned char);
+extern void lcd_char(unsigned char);
+extern void lcd_str(unsigned char *);
+extern void lcd_display(unsigned char);
+extern void delay_ms(unsigned int);
